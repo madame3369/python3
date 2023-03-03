2ed598826351138e36bce169177810360b238a5d
madame3369 <akeka0303@naver.com>
2023-03-03 15:52:28 +0900

    파이썬 학습1일차

---
+++ 몫구하기.py
@@ -0,0 +1,15 @@
+def solution(num1, num2):
+    answer = num1//num2
+    return answer
+print(solution(12, 3))
+
+#answer = num1/num2
+#return int(answer)
+
+#return divmod(num1, num2)[0]
+
+
+
+#import math
+#def solution(num1, num2):
+# return math.floor(num1/num2)
